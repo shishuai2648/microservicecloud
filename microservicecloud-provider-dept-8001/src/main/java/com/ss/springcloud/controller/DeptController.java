8610deb8802575642d42dbcb9834e3e2ffeb677d
shishuai <shishuai@163.com>
2018-11-16 15:56:19 +0800

    Zuul组件添加

--- microservicecloud-provider-dept-8001/src/main/java/com/ss/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/ss/springcloud/controller/DeptController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -19,6 +20,7 @@ public class DeptController {
     DeptService deptService;
     @Autowired
     private DiscoveryClient client;
+
 
     @PostMapping("/add")
     public boolean add(@RequestBody Dept dept){
